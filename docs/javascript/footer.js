8c85cb8ec230901fb872873a6ad92dc06ac9fb8e
Muhammad Talha <talha18513@gmail.com>
2025-09-29 21:37:34 -0700

    first commit

--- docs/javascript/footer.js
+++ docs/javascript/footer.js
@@ -15,7 +15,7 @@ footer.innerHTML = `
       </p>
 
       <div class="mt-7 flex items-center gap-3 flex-wrap">
-        <a href="tel:+4915121711109"
+        <a href="tel:+015126004444"
            class="tracking-wider py-2 px-7 text-center rounded-full border-2 border-[#0165a1] bg-[#0165a1] text-white font-medium hover:opacity-90 transition">
           Jetzt anrufen
         </a>
@@ -30,7 +30,7 @@ footer.innerHTML = `
     <div data-aos="fade-up" data-aos-delay="100">
       <h3 class="text-2xl text-zinc-900 font-semibold">Seiten</h3>
       <ul class="mt-5 space-y-3 text-zinc-700 font-medium">
-        <li><a href="/index.html" class="hover:text-[#0165a1]">Home</a></li>
+        <li><a href="/index.html" class="hover:text-[#0165a1]">Startseite</a></li>
         <li><a href="/about.html" class="hover:text-[#0165a1]">über</a></li>
         <li><a href="/services.html" class="hover:text-[#0165a1]">Leistungen </a></li>
         <li><a href="/location.html" class="hover:text-[#0165a1]">Standorte </a></li>
@@ -79,7 +79,7 @@ footer.innerHTML = `
   <!-- Legal / bottom bar -->
   <div class="mt-8 border-t border-zinc-200 pt-4">
     <p class="text-center text-zinc-600 mt-4">
-      Copyright © <span id="copyright"></span> Kanalwurm Rohrreinigung. Alle Rechte vorbehalten.
+      Copyright © <span id="copyright"></span> Sanitar24. Alle Rechte vorbehalten.
     </p>
   </div>
 </footer>
